f76d4744e96b76071fd0d13b02efe63bca718d99
chandan0134 <chandanma39@gmail.com>
2022-11-02 02:01:20 +0530

    commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+engine.say("i am johnny")
+engine.say("what can i do for you")
+engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            if 'johnny' in command:
+                command = command.replace('johnny', '')
+                print(command)
+    except:
+        pass
+    return 
+
+def run_johnny():
+    command=take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    
+
+
+
+
+
